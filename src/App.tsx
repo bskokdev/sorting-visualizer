72cd582e6a2e5b0aa37697bf1e891b61630b0e2b
Boris Skok <skokcmd@gmail.com>
2023-02-11 01:21:52 +0100

    tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      {/*  Title */}
+      <h1 className="text-4xl font-extrabold uppercase text-gray-700 py-3">
+        Sorting visualizer
+      </h1>
+      {/*  Controls for choosing sorting algorithm */}
+      <div>to be done</div>
+    </div>
+  );
+}
+
+export default App;
