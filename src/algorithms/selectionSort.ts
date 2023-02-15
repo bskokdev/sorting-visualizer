2cb0d8db9876e2d1895c79e81a0cf5012c5410dc
skokcmd <skokcmd@gmail.com>
2023-02-15 15:40:11 +0100

    added selection sort

--- src/algorithms/selectionSort.ts
+++ src/algorithms/selectionSort.ts
@@ -1,10 +1,45 @@
 import { SortingAlgorithm, SortingAlgorithmProps } from "../types";
+import { sleep } from "../utils";
 
+/**
+ * Selection Sort - O(n^2) time complexity
+ * @param {SortingAlgorithmProps} { bars, setBars, speed, cleanup }
+ */
 export const selectionSort: SortingAlgorithm = async ({
   bars,
   updateBars,
   speed,
   cleanup,
 }: SortingAlgorithmProps): Promise<void> => {
-  // todo implement selection sort
+  const sortedBars = [...bars];
+  for (let i = 0; i < sortedBars.length; ++i) {
+    let minIndex = i;
+    sortedBars[i].color = "#b91c1c";
+    updateBars(sortedBars);
+    await sleep(speed);
+    // find the minimum element in the unsorted array
+    for (let j = i + 1; j < sortedBars.length; ++j) {
+      sortedBars[j].color = "#0369a1";
+      updateBars(sortedBars);
+      await sleep(speed * 5);
+      sortedBars[j].color = "coral";
+      updateBars(sortedBars);
+      if (sortedBars[j].weight < sortedBars[minIndex].weight) {
+        minIndex = j;
+      }
+    }
+    sortedBars[minIndex].color = "green";
+    updateBars(sortedBars);
+    await sleep(speed * 100);
+
+    sortedBars[i].color = "coral";
+    sortedBars[minIndex].color = "coral";
+    updateBars(sortedBars);
+    await sleep(speed);
+    // swap at the end of each iteration
+    const temp = sortedBars[i];
+    sortedBars[i] = sortedBars[minIndex];
+    sortedBars[minIndex] = temp;
+  }
+  await cleanup(sortedBars);
 };
