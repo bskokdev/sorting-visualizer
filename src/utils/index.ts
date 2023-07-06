8ca0e87d00e0925bf8097096971efc1ea3ebb9ef
Boris Skok <skokcmd@gmail.com>
2023-07-06 12:01:47 +0200

    updated docs & formatting in global utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { Bar } from "../types";
+import {Bar} from "../types";
 
 /**
  * This file contains utility functions that are used in multiple places
@@ -9,19 +9,19 @@ import { Bar } from "../types";
  * @param bars - array of bars
  * @param color - hex color
  * @param speed - in ms
- * @param setBars - function to update the state
+ * @param updateBars
  */
 export async function setBarArrayColor(
-  bars: Array<Bar>,
-  color: string,
-  speed: number,
-  updateBars: (bars: Bar[]) => void
+    bars: Array<Bar>,
+    color: string,
+    speed: number,
+    updateBars: (bars: Bar[]) => void
 ) {
-  for (let i = 0; i < bars.length; ++i) {
-    bars[i].color = color;
-    updateBars([...bars]);
-    await sleep(speed);
-  }
+    for (let i = 0; i < bars.length; ++i) {
+        bars[i].color = color;
+        updateBars([...bars]);
+        await sleep(speed);
+    }
 }
 
 /**
@@ -30,20 +30,20 @@ export async function setBarArrayColor(
  * @returns Promise<void>
  */
 export function sleep(milliseconds: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
 export function getRandomNumber(from: number, to: number): number {
-  return Math.floor(Math.random() * (to - from + 1) + from);
+    return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
 /**
  * Returns the number of bars to be displayed on the screen based on the screen size
  */
 export function getMaxBarsForScreen(): number {
-  const width = window.innerWidth;
-  if (width < 768) return 18;
-  if (width < 1024) return 40;
-  if (width < 1440) return 70;
-  return 100;
+    const width = window.innerWidth;
+    if (width < 768) return 18;
+    if (width < 1024) return 40;
+    if (width < 1440) return 70;
+    return 100;
 }
