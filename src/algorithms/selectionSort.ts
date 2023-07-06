5cd1fc11b516d0e5849c869ed9347857f018e205
Boris Skok <skokcmd@gmail.com>
2023-07-06 12:03:25 +0200

    cleaned selection sort

--- src/algorithms/selectionSort.ts
+++ src/algorithms/selectionSort.ts
@@ -1,45 +1,43 @@
-import { SortingAlgorithm, SortingAlgorithmProps } from "../types";
-import { sleep } from "../utils";
+import {SortingAlgorithm, SortingAlgorithmProps} from "../types";
+import {sleep} from "../utils";
+import {swap} from "./utils";
 
 /**
  * Selection Sort - O(n^2) time complexity
  * @param {SortingAlgorithmProps} { bars, setBars, speed, cleanup }
  */
 export const selectionSort: SortingAlgorithm = async ({
-  bars,
-  updateBars,
-  speed,
-  cleanup,
-}: SortingAlgorithmProps): Promise<void> => {
-  const sortedBars = [...bars];
-  for (let i = 0; i < sortedBars.length; ++i) {
-    let minIndex = i;
-    sortedBars[i].color = "#b91c1c";
-    updateBars(sortedBars);
-    await sleep(speed);
-    // find the minimum element in the unsorted array
-    for (let j = i + 1; j < sortedBars.length; ++j) {
-      sortedBars[j].color = "#0369a1";
-      updateBars(sortedBars);
-      await sleep(speed);
-      sortedBars[j].color = "coral";
-      updateBars(sortedBars);
-      if (sortedBars[j].weight < sortedBars[minIndex].weight) {
-        minIndex = j;
-      }
-    }
-    sortedBars[minIndex].color = "green";
-    updateBars(sortedBars);
-    await sleep(speed);
+                                                          bars,
+                                                          updateBars,
+                                                          speed,
+                                                          cleanup,
+                                                      }: SortingAlgorithmProps): Promise<void> => {
+    const sortedBars = [...bars];
+    for (let i = 0; i < sortedBars.length; ++i) {
+        let minIndex = i;
+        sortedBars[i].color = "#b91c1c";
+        updateBars(sortedBars);
+        await sleep(speed);
+        // find the minimum element in the unsorted array
+        for (let j = i + 1; j < sortedBars.length; ++j) {
+            sortedBars[j].color = "#0369a1";
+            updateBars(sortedBars);
+            await sleep(speed);
+            sortedBars[j].color = "coral";
+            updateBars(sortedBars);
+            if (sortedBars[j].weight < sortedBars[minIndex].weight) {
+                minIndex = j;
+            }
+        }
+        sortedBars[minIndex].color = "green";
+        updateBars(sortedBars);
+        await sleep(speed);
 
-    sortedBars[i].color = "coral";
-    sortedBars[minIndex].color = "coral";
-    updateBars(sortedBars);
-    await sleep(speed);
-    // swap at the end of each iteration
-    const temp = sortedBars[i];
-    sortedBars[i] = sortedBars[minIndex];
-    sortedBars[minIndex] = temp;
-  }
-  await cleanup(sortedBars);
+        sortedBars[i].color = "coral";
+        sortedBars[minIndex].color = "coral";
+        updateBars(sortedBars);
+        await sleep(speed);
+        swap(sortedBars, i, minIndex)
+    }
+    await cleanup(sortedBars);
 };
