d2a10a5c2a1f5b11b1775af87ea8ff117d7fdbbd
Boris Skok <skokcmd@gmail.com>
2023-07-06 12:01:24 +0200

    applied prettier on sorting reducer

--- src/reducers/sorterReducer.ts
+++ src/reducers/sorterReducer.ts
@@ -1,49 +1,49 @@
-import { speedOptions } from "../constants";
-import { Bar, Sorter, SortingAlgorithmType } from "../types";
-import { getMaxBarsForScreen } from "../utils";
+import {speedOptions} from "../constants";
+import {Bar, Sorter, SortingAlgorithmType} from "../types";
+import {getMaxBarsForScreen} from "../utils";
 
 /**
- * This file contains reducer for the sorter state and coresponding types
+ * This file contains reducer for the sorter state and corresponding types
  */
 
 export const INITIAL_STATE: Sorter = {
-  algo: "bubble",
-  bars: [],
-  speed: speedOptions[1].value as number,
-  isSorting: false,
-  currInputSize: getMaxBarsForScreen(),
+    algo: "bubble",
+    bars: [],
+    speed: speedOptions[1].value as number,
+    isSorting: false,
+    currInputSize: getMaxBarsForScreen(),
 };
 
 type SorterPayload =
-  | SortingAlgorithmType
-  | Bar[]
-  | boolean
-  | { field: string; value: string | number };
+    | SortingAlgorithmType
+    | Bar[]
+    | boolean
+    | { field: string; value: string | number };
 
 export interface SorterAction {
-  type: SorterActionType;
-  payload: SorterPayload;
+    type: SorterActionType;
+    payload: SorterPayload;
 }
 
 export type SorterActionType =
-  | "SET_ALGO"
-  | "SET_BARS"
-  | "SET_IS_SORTING"
-  | "HANDLE_INPUT_CHANGE";
+    | "SET_ALGO"
+    | "SET_BARS"
+    | "SET_IS_SORTING"
+    | "HANDLE_INPUT_CHANGE";
 
 type UpdateSorterState = (state: Sorter, payload: any) => Sorter;
 
 // reducer for the sorter state which takes the current state and an action and returns the new state based on the action type
 export function sorterReducer(state: Sorter, action: SorterAction) {
-  const { type, payload } = action;
-  const actions: Record<SorterActionType, UpdateSorterState> = {
-    SET_ALGO: (state, payload) => ({ ...state, algo: payload }),
-    SET_BARS: (state, payload) => ({ ...state, bars: payload }),
-    SET_IS_SORTING: (state, payload) => ({ ...state, isSorting: payload }),
-    HANDLE_INPUT_CHANGE: (state, payload) => ({
-      ...state,
-      [payload.field]: payload.value,
-    }),
-  };
-  return actions[type](state, payload);
+    const {type, payload} = action;
+    const actions: Record<SorterActionType, UpdateSorterState> = {
+        SET_ALGO: (state, payload) => ({...state, algo: payload}),
+        SET_BARS: (state, payload) => ({...state, bars: payload}),
+        SET_IS_SORTING: (state, payload) => ({...state, isSorting: payload}),
+        HANDLE_INPUT_CHANGE: (state, payload) => ({
+            ...state,
+            [payload.field]: payload.value,
+        }),
+    };
+    return actions[type](state, payload);
 }
